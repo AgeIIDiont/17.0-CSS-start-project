f7742c23682c129d0577ced42efa553decc14dc7
Your Name <you@example.com>
2025-11-13 19:41:31 +0700

    17.0-5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import logo from "../assets/logo-tuhoc.png";
+//import "./Header.css";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>CSS trong React</h1>
-      <p>Tìm hiểu về css trong react cho người mới.</p>
+      <h1 className={classes.paragraph}>CSS trong React</h1>
+      <h2 className={classes["my-paragraph"]}>CSS trong React</h2>
+      <p style = {{
+        color: "#555",
+        backgroundColor: "#f0f0f0",
+      }}>Tìm hiểu về css trong react cho người mới.</p>
     </header>
   );
 }
